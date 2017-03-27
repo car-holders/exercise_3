bfca47dc352acf6b4ea97436645b653862e4a2be
jieli <jieli280495@gmail.com>
2017-03-27 21:03:39 +0200

    add test for vehicle db add ecu

---
+++ socam/test/no/ntnu/fp/storage/VehicleDbStorageITest.java
@@ -0,0 +1,53 @@
+package no.ntnu.fp.storage;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import no.ntnu.fp.gui.FactoryProjectPanel;
+import no.ntnu.fp.gui.NewVehiclePanel;
+import no.ntnu.fp.model.Ecu;
+import no.ntnu.fp.model.Vehicle;
+
+public class VehicleDbStorageITest extends TestCase {
+
+	private VehicleDbStorage vehicledb;
+	private Vehicle vehicle;
+	private NewVehiclePanel nvp;
+	private Ecu ecu;
+	private ArrayList<String> messages;
+	
+	public VehicleDbStorageITest() throws Exception {
+		setUp();
+	}
+
+	protected void setUp() throws Exception {
+		//Set up vdb
+		vehicledb = new VehicleDbStorage();
+		
+		//Set up Ecu
+		ecu = new Ecu(57);
+		
+		//Set up v
+		vehicle = new Vehicle();
+		vehicle.addEcu(ecu);
+		vehicle.setVehicleID("44");
+		
+		
+		//Set up nvp
+		nvp = new NewVehiclePanel(new FactoryProjectPanel());
+		nvp.setModel(vehicle);
+		
+	}
+	
+	protected void tearDown() throws Exception{
+		
+	}
+
+	public void testAddEcus(){
+		messages = vehicledb.addEcus(vehicle, nvp);
+		assertEquals("Vehicle updated", messages.get(0));
+		
+	}
+
+	
+}
