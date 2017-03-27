5443a693ff332277ad6d327ac6cafb6687959b73
sklirg <hlsolbjorg@gmail.com>
2017-03-27 17:48:28 +0200

    Make a working test case

--- socam/test/no/ntnu/fp/storage/dummyTest.java
+++ socam/test/no/ntnu/fp/storage/dummyTest.java
@@ -3,5 +3,7 @@ package no.ntnu.fp.storage;
 import junit.framework.TestCase;
 
 public class dummyTest extends TestCase {
-
+    public void testOneEqualsOne() {
+        assertEquals(1, 1);
+    }
 }
